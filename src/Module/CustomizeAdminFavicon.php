09165335ab3ac403bd817da026786d2f689414b0
Gifford Nowland <hi@giffordnowland.com>
2017-09-18 12:41:37 -0700

    port admin favicon

---
+++ src/Module/CustomizeAdminFavicon.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Gnowland\JetFuel\Module;
+
+use Gnowland\JetFuel\Instance;
+
+/**
+ * Module: customize-admin-favicon
+ *
+ * Adds section to Customizer to customize admin-favicon
+ *
+ * @example jetfuel('customize-admin-favicon');
+ *
+ * @link https://developer.wordpress.org/reference/hooks/customize_register/
+ * @link https://developer.wordpress.org/reference/hooks/site_icon_meta_tags/
+ *
+ * @package WordPress
+ * @subpackage WPJetFuel
+ * @since 0.2.0
+ */
+class CustomizeAdminFavicon extends Instance {
+  public function run() {
+      $this->setup()->hook();
+  }
+
+  protected function setup() {
+      $this->setDefaultConfig('');
+      return $this;
+  }
+
+  protected function hook() {
+    // add customizer setting to choose a favicon
+    add_action( 'customize_register', [$this, 'addAdminFavicon']);
+    // add favicon to admin pages
+    if (is_admin()) {
+      add_filter ('site_icon_meta_tags', [$this, 'useAdminFavicon']);
+    }
+  }
+
+  public function addAdminFavicon($wp_customize) {
+
+    // Register customizer setting
+    $wp_customize->add_setting( 'jetfuel_admin_favicon', array(
+      'capability' => 'manage_options',
+      'type'       => 'option',
+      'default'    => get_option( 'jetfuel_admin_favicon', '' ),
+      'transport'  => 'postMessage', // Previewed with JS in the Customizer controls window.
+     ) );
+
+    // Add image uploader
+    $wp_customize->add_control( new \WP_Customize_Site_Icon_Control( $wp_customize, 'admin_favicon', array(
+      'label'       => __( 'Admin Favicon', 'jetfuel' ),
+      'description' => __( 'Favicons must be square, .ico format. 48x48px grayscale image suggested.', 'jetfuel' ),
+      'description' => sprintf(
+        /* translators: %s: site icon size in pixels */
+        __( 'The Admin Favicon is used as a browser icon for the admin panel. Icons <strong>MUST BE .ico FORMAT</strong>, square, exactly %s pixels wide and tall, and a different color from your public Site Icon (grayscale suggested)', 'jetfuel' ),
+        '<strong>48</strong>'
+      ),
+      'section'     => 'title_tagline',
+      'settings'    => 'jetfuel_admin_favicon',
+      'priority'    => 70,
+      'height'      => 48,
+      'width'       => 48
+    ) ) );
+  }
+
+  public function useAdminFavicon($meta_tags) {
+    $favicon_url = get_option( 'admin_favicon', '' );
+    $meta_tags = array('<link rel="shortcut icon" href="' . esc_url($favicon_url) . '" />');
+    return $meta_tags;
+  }
+}
