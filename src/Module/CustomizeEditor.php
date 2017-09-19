0462325e4c38124844b827a14609a221fdf45bae
Gifford Nowland <hi@giffordnowland.com>
2017-09-19 15:12:23 -0700

    prevent tinymce infinite resize

--- src/Module/CustomizeEditor.php
+++ src/Module/CustomizeEditor.php
@@ -41,6 +41,8 @@ class CustomizeEditor extends Instance {
         add_filter('tiny_mce_before_init', [$this, 'removeH1']);
         // Add formats to the style select
         add_filter('tiny_mce_before_init', [$this, 'customizeStyleFormats']);
+        // Inject <style> into TinyMCE <iframe> <head> to fix infinite-height bug
+        add_action('admin_head', [$this, 'preventInfiniteResize'], 99);
     }
 
     public function moveHelp($buttons) {
@@ -169,4 +171,33 @@ class CustomizeEditor extends Instance {
 
         return $settings;
     }
+
+    public function preventInfiniteResize() {
+      // Append classes to TinyMCE <iframe> body to target article styles
+      ?>
+      <script>
+        var theFrame;
+        window.onload = function() {
+          theFrame = document.getElementsByTagName("iframe")[0];
+          if (!theFrame) {
+            window.setTimeout(function(){
+              theFrame = document.getElementsByTagName("iframe")[0]
+            }, 1000);
+          } else {
+            if (theFrame.id !== "content_ifr"){ return; }
+            var theFrameDocument = theFrame.contentDocument || theFrame.contentWindow.document;
+
+            // Add <style> to <head>
+            var frmhead = theFrameDocument.head;
+            frmhead.innerHTML += '<style>html,body{height:auto!important;}</style>';
+
+            // Add class to <body>
+            var frmbody = theFrameDocument.body;
+            frmbody.className += " entry-content";
+          }
+        };
+      </script>
+      <?php
+    }
+
 }
