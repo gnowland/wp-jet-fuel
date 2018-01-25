d395b81301e9397971d8d3e179337505b6fde494
Gifford Nowland <hi@giffordnowland.com>
2018-01-25 06:15:54 -0500

    allow passing of custom menu order

--- src/Module/ReorderMenu.php
+++ src/Module/ReorderMenu.php
@@ -9,7 +9,8 @@ use Gnowland\JetFuel\Instance;
  *
  * Hooks into menu_order to reorder menu items
  *
- * @example jetfuel('reorder-menu');
+ * @example jetfuel('reorder-menu', $config(array));
+ * @param   array|string $config
  *
  * @link https://developer.wordpress.org/reference/hooks/admin_menu/
  * @link https://developer.wordpress.org/reference/hooks/custom_menu_order/
@@ -25,28 +26,7 @@ class ReorderMenu extends Instance {
     }
 
     protected function setup() {
-        $this->setDefaultConfig([]);
-        return $this;
-    }
-
-    protected function hook() {
-        // Add extra spacers to menu
-        add_action('admin_menu', [$this, 'addSpacers'], 11);
-        // Rearrange the admin menu
-        add_action('custom_menu_order', [$this, 'reorderMenu']);
-        add_action('menu_order', [$this, 'reorderMenu']);
-    }
-
-    public function addSpacers() {
-        global $menu;
-        $menu[] = array( '', 'read', 'separator-core', '', 'wp-menu-separator' ); // Add separator-core
-        $menu[] = array( '', 'read', 'separator-plugins', '', 'wp-menu-separator' ); // Add separator-plugins
-    }
-
-    public function reorderMenu($menu_ord) {
-        if (!$menu_ord) return true;
-
-        $menu_ord = array(
+        $this->setDefaultConfig([
             // Dashboard
             'index.php', // Dashboard
             'themes.php', // Appearance
@@ -77,7 +57,28 @@ class ReorderMenu extends Instance {
             // Everything else...
             'separator-last', // Last separator
             // ...automatically appears here
-        );
+        ]);
+        return $this;
+    }
+
+    protected function hook() {
+        // Add extra spacers to menu
+        add_action('admin_menu', [$this, 'addSpacers'], 11);
+        // Rearrange the admin menu
+        add_action('custom_menu_order', [$this, 'reorderMenu']);
+        add_action('menu_order', [$this, 'reorderMenu']);
+    }
+
+    public function addSpacers() {
+        global $menu;
+        $menu[] = array( '', 'read', 'separator-core', '', 'wp-menu-separator' ); // Add separator-core
+        $menu[] = array( '', 'read', 'separator-plugins', '', 'wp-menu-separator' ); // Add separator-plugins
+    }
+
+    public function reorderMenu($menu_ord) {
+        if (!$menu_ord) return true;
+
+        $menu_ord = $this->config;
 
         return $menu_ord;
     }
