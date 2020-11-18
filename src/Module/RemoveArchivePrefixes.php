e054cd87af8c68b833bf20a65d36fb0363c7cc68
Gifford Nowland <hi@giffordnowland.com>
2020-11-18 07:19:15 -0500

    Update RemoveArchivePrefixes.php

--- src/Module/RemoveArchivePrefixes.php
+++ src/Module/RemoveArchivePrefixes.php
@@ -39,11 +39,10 @@ class RemoveArchivePrefixes extends Instance {
           $title = single_tag_title( '', false );
         } elseif ( is_author() ) {
           $title = '<span class="vcard">' . get_the_author() . '</span>' ;
+        } elseif ( is_tax() ) { //for custom post types
+          $title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
         } elseif ( is_post_type_archive() ) {
           $title = post_type_archive_title( '', false );
-        } elseif ( is_tax() ) {
-          $tax = get_taxonomy( get_queried_object()->taxonomy );
-          $title = single_term_title( '', false );
         }
         return $title;
     }
